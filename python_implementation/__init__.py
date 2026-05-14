b75cb4ab521072695ad0a335639e53f5b3e981f4
shinyatlas <mannyoso07@gmail.com>
2026-05-13 20:52:02 -0400

    edited file structure to make space for a cpp variant

---
+++ python_implementation/__init__.py
@@ -0,0 +1,11 @@
+"""python_implementation package
+
+This package contains the Python implementation moved from the repository root.
+"""
+
+__all__ = [
+    "calibrate_simulator",
+    "generate_directional_augmentation",
+    "load_real_spy_csv",
+    "BoidsMarketSimulator",
+]
